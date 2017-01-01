9b8fdd43494fbd043357969d9727124f81cf9677
yu kawase <kawase@aloe-project.com>
2017-01-01 15:33:56 +0900

    Initial Commit

---
+++ davium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  davium
+//
+//  Created by kawase yu on 2017/01/01.
+//  Copyright © 2017年 Davium inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
